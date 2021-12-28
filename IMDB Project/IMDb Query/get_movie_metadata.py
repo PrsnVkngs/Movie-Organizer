e6df43ac16ca242566c8fc217e7c351bc906a52c
PrsnVkngs <oskaraattar@gmail.com>
2021-12-27 17:40:32 -0800

    Updated the media info class
    Added the ability for the get_movie_metadata class to be able to return the text (subtitle) track data in a movie file. Also fixed the return type so that the function properly returns all data.

--- IMDB Project/IMDb Query/get_movie_metadata.py
+++ IMDB Project/IMDb Query/get_movie_metadata.py
@@ -2,55 +2,55 @@ from pymediainfo import MediaInfo as mi
 
 
 def get_track_info(movie):
+    """
+    Given the directory location of a movie, this function will parse the file into MediaInfo and return
+    the available video, audio and text track details.
+    :param movie:
+    :return: A list of data in dict form for the movie file. Video track is element 1, Audio track is element 2,
+    Text track is element 3. There will be a string saying "not available" if media info does not find a track for that
+    type.
+    """
     mediainfo_dat = mi.parse(movie)
 
     video_track = []
     audio_track = []
+    text_track = []
 
     video_track_count = 0
     audio_track_count = 0
+    text_track_count = 0
 
     for track in mediainfo_dat.tracks:
         if track.track_type == "Video":
             video_track_count += 1
-            video_track.append(
-                {
-                    "track_no": video_track_count,
-                    "height": track.height,
-                    "width": track.width,
-                    "format": track.format,
-                    "duration": track.other_duration[0],
-                    "bit_rate": track.bit_rate,
-                    "simple_bit_rate": track.other_bit_rate,
-                    "frame_rate": track.frame_rate,
-                    "bit_depth": track.bit_depth,
-                    "simple_bit_depth": track.other_bit_depth
-                }
-            )
+            video_track.append(sorted(track.to_data()))
+
         elif track.track_type == "Audio":
             audio_track_count += 1
-            audio_track.append(
-                {
-                    "track_no": audio_track_count,
-                    "format": track.format,
-                    "alt_format": track.other_format,
-                    "easy_format": track.commercial_name,
-                    "duration": track.other_duration[0],
-                    "bit_rate": track.bit_rate,
-                    "easy_bit_rate": track.other_bit_rate[0],
-                    "bit_rate_mode": track.bit_rate_mode,
-                    "channels": track.channel_s,
-                    "sampling_rate": track.sampling_rate,
-                    "easy_sampling_rate": track.other_sampling_rate[0],
-                    "compression_mode": track.compression_mode,
-                    "title": track.title,
-                    "language": track.language,
-                    "easy_language": track.other_language[0]
-                }
-            )
+            audio_track.append(sorted(track.to_data()))
 
-    print(video_track)
-    print(audio_track)
+        elif track.track_type == "Text":
+            text_track_count += 1
+            text_track.append(sorted(track.to_data()))
+
+    movie_metadata = []
+
+    if video_track_count == 0:
+        movie_metadata.append("not available")
+    else:
+        movie_metadata.append(video_track)
+
+    if audio_track_count == 0:
+        movie_metadata.append("not available")
+    else:
+        movie_metadata.append(audio_track)
+
+    if text_track_count == 0:
+        movie_metadata.append("not available")
+    else:
+        movie_metadata.append(text_track)
+
+    return movie_metadata
 
 
 if __name__ == '__main__':
